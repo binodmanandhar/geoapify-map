488e2626b6969ef646cbb8bc370aac48f03b7bb4
Binod Manandhar <binodmanandhar7@gmail.com>
2026-05-25 19:59:21 +1000

    added npm install --save-dev copy-webpack-plugin package

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/app.ts",
@@ -27,6 +28,9 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "app.css", to: "" }],
     }),
   ],
   mode: "development",
